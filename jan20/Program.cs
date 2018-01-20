c5df1693a122d73780120423101a65e3ae70697d
Bence Vér <szeraf@gmail.com>
2018-01-20 18:05:00 +0100

    jan20v1

---
+++ jan20/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace jan20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Add meg a telefonszamod: ");
+            string telszam = Console.ReadLine();
+
+            string elsoketto = telszam.Substring(0,2);
+            string elsonegy = telszam.Substring(0,4);
+            if (elsoketto == "06")
+            {
+                telszam = "+36" + telszam.Substring(2);
+            }
+            else if (elsonegy == "0036")
+            {
+                telszam = "+36" + telszam.Substring(4);
+            }
+
+            int perhely = telszam.IndexOf("/");
+            if (perhely != (-1))
+            {
+                telszam = telszam.Replace("/","");
+                telszam = telszam.Replace("(","");
+                telszam = telszam.Replace(")","");
+                telszam = telszam.Replace(" ","");
+                telszam = telszam.Replace("-","");
+            }
+
+            if (telszam.Length == 9 || telszam.Length == 8)
+            {
+                telszam = "+36" + telszam;
+            }
+
+            telszam = telszam.Insert(5,"/");
+            System.Console.WriteLine(telszam);
+            telszam = telszam.Remove(5,1);
+
+            System.Console.WriteLine(telszam);
+
+            if (telszam.Contains("+361"))
+            {
+                System.Console.WriteLine("budapesti");
+            }else
+            {
+                System.Console.WriteLine("nem budapesti");
+            }
+
+            string evszakok = "tavasz,nyar,osz,tel";
+            string[] evszakokT = evszakok.Split(',');
+            System.Console.WriteLine(evszakokT[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ palindrom/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Kerek egy mondatot: ");
+            string be = Console.ReadLine();
+
+            be = be.ToLower();
+
+            be = be.Replace(" ","");
+            be = be.Replace(".","");
+            be = be.Replace(",","");
+            be = be.Replace("!","");
+            be = be.Replace("?","");
+
+            bool pali = true;
+            for (int i = 0; i < be.Length/2; i++)
+            {
+                if (be[i] != be[be.Length-i-1]) pali = false;
+            }
+
+            if (pali)
+            {
+                System.Console.WriteLine("Palindrom.");
+            }
+            else
+            {
+                System.Console.WriteLine("Nem palindrom.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
