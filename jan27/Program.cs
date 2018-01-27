113141135c6cb11ebf01f793f1f65560cfc1c8e3
Bence Vér <szeraf@gmail.com>
2018-01-27 16:21:42 +0100

    init

---
+++ jan27/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace jan27
+{
+    // filekezelés
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs1 = new FileStream("bemenet.txt",FileMode.Open);
+            StreamReader sr1 = new StreamReader(fs1);
+
+            int sorszam = 1;
+
+            while (!sr1.EndOfStream)
+            {
+                System.Console.WriteLine(sorszam + ".sor: "+ sr1.ReadLine());
+                sorszam++;
+            }
+
+            sr1.Close();
+            fs1.Close();
+            
+            // FileMode: Create(felülír), CreateNew(ha már létezik error), Append(hozzáfűz) -- csak írható
+            //           Open(csak olvasható)
+            //           OpenOrCreate(írható és olvasható)
+            FileStream fs2 = new FileStream("kimenet.txt",FileMode.Create);
+            StreamWriter sw1 = new StreamWriter(fs2);
+
+            for (int i = 1; i <= 100; i++)
+            {
+                sw1.Write(i + " ");
+            }
+
+            sw1.Close();
+            fs2.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ osztok/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace osztok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // FileMode: Create(felülír), CreateNew(ha már létezik error), Append(hozzáfűz) -- csak írható
+            //           Open(csak olvasható)
+            //           OpenOrCreate(írható és olvasható)
+            FileStream fs1 = new FileStream("kimenet.txt",FileMode.Create);
+            StreamWriter sw1 = new StreamWriter(fs1);
+
+            System.Console.Write("Az elso hany termeszetes szam valodi osztoit szamoljam ki? ");
+            int darab = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= darab; i++)
+            {
+                sw1.Write(i + ": ");
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sw1.Write(j + " ");
+                    }
+                }
+                sw1.WriteLine();
+            }
+
+            sw1.Close();
+            fs1.Close();
+
+            //Console.ReadLine();
+        }
+    }
+}
